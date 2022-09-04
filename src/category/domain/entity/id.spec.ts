7ddf04de5257230864052478b15bde940ef15f84
Nilton Morais <evenilton.morais@kangu.com.br>
2022-09-04 15:12:11 -0300

    spy teste 2

--- src/category/domain/entity/id.spec.ts
+++ src/category/domain/entity/id.spec.ts
@@ -6,8 +6,10 @@ test("Deve criar um id", function(){
 });
 
 test("Deve hidratar um id", function(){
+    const validateSpy = jest.spyOn(Id.prototype as any, 'validate');
     const id = new Id('723b8dcd-e9f2-4c33-8758-8bdcb8a862c5');
     expect(id.value).toBe('723b8dcd-e9f2-4c33-8758-8bdcb8a862c5');
+    expect(validateSpy).toHaveBeenCalled();
 });
 
 test('Deve retornar excessão ao criar id inválido', () => {
